edc77393f2288dbb41e697a004b5cb904c912e0c
James Yanyuk <jyanyuk@umass.edu>
2014-11-13 10:58:58 -0500

    Set up routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET api */
-router.get('/', function(req, res) {
-    res.send('test');
+router.post('/objects', function(req, res) {
+    // create new scienctist record with unique id
+    // store in database
+    // return record
+});
+
+router.put('/objects/:uid', function(req, res) {
+
+});
+
+router.get('/objects/:uid', function(req, res) {
+
+});
+
+router.delete('/objects/:uid', function(req, res) {
+
 });
 
 module.exports = router;
