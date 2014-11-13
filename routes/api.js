4c5ee2caa071f057eb2ee241237fadee3ac8d21d
James Yanyuk <jyanyuk@umass.edu>
2014-11-13 12:32:26 -0500

    Working on HTTP POST

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var scientist = require('scientist');
+
 router.post('/objects', function(req, res) {
     // create new scienctist record with unique id
     // store in database
     // return record
+    var genUID = Math.random().toString().substr(2, 6);
+    scientist.add(genUID, 'Andrey', 'Kolmogorov', '25 April 1903');
+    res.redirect('/objects/' + genUID);
 });
 
 router.put('/objects/:uid', function(req, res) {
