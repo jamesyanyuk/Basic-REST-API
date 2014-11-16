12acc3cfc9fcb9f8e72d6bd28a3aa16ebaad1d17
James Yanyuk <jyanyuk@umass.edu>
2014-11-16 02:46:20 -0500

    Testing heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('hi');
+  res.render('index', { title : 'Basic REST API',
+                        message : 'Instructions will be added soon!' });
 });
 
 module.exports = router;
