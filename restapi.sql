1e6a07c6c6c88faa28a15a6fd28b0328f558e261
James Yanyuk <jyanyuk@umass.edu>
2014-11-16 02:59:13 -0500

    Testing heroku

--- restapi.sql
+++ restapi.sql
@@ -6,12 +6,6 @@ create table objects (
     primary key (uid)
 );
 
-insert into objects values ('000001',
-    '{"uid" : "000001","firstname" : "Andrey","lastname" : "Kolmogorov","dob" : "25 April 1903"}'
-);
-insert into objects values ('000002',
-    '{"uid" : "000002","firstname" : "11","lastname" : "12","dob" : "13"}'
-);
-insert into objects values ('000003',
-    '{"uid" : "000003","firstname" : "21","lastname" : "22","dob" : "23"}'
-);
+insert into objects values ('000001', '{"uid" : "000001","firstname" : "Andrey","lastname" : "Kolmogorov","dob" : "25 April 1903"}');
+insert into objects values ('000002', '{"uid" : "000002","firstname" : "11","lastname" : "12","dob" : "13"}');
+insert into objects values ('000003', '{"uid" : "000003","firstname" : "21","lastname" : "22","dob" : "23"}');
