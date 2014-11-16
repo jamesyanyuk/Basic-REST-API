752f554de39121b9011ce98830bb3f56043f3320
James Yanyuk <jyanyuk@umass.edu>
2014-11-15 21:59:18 -0500

    Added PUT and GET functionality

--- routes/api.js
+++ routes/api.js
@@ -31,16 +31,58 @@ router.post('/objects', function(req, res) {
     obj.uid = uid;
 
     db.addObject(obj);
-    //console.log(db.getObject(uid));
+    db.getObject(uid, function(err, obj) {
+        if(err)
+            console.log(err);
+        else
+            console.log(obj);
+    });
     res.json(obj);
 });
 
 router.put('/objects/:uid', function(req, res) {
+    // Check if received data is a JSON object
+    try {
+        JSON.parse(JSON.stringify(req.body));
+        if(Object.keys(req.body).length === 0)
+            throw Error();
+    } catch(e) {
+        var err = {
+            "verb" : "PUT",
+            "url" : "api/objects/<uid>",
+            "message" : "Not a JSON object"
+        };
+        res.json(err);
+        return;
+    }
 
+    db.updateObject(req.param('uid'), req.body, function(err, obj) {
+        if(err){
+            var error = {
+                "verb" : "PUT",
+                "url" : "api/objects/<uid>",
+                "message" : "Object with specified UID doesn't exist"
+            };
+            res.json(error);
+        }else{
+            res.json(obj);
+        }
+    });
 });
 
 router.get('/objects/:uid', function(req, res) {
-
+    db.getObject(req.param('uid'), function(err, obj) {
+        if(err){
+            var error = {
+                "verb" : "GET",
+                "url" : "api/objects/<uid>",
+                "message" : "Object with specified UID doesn't exist"
+            };
+            res.json(error);
+        }else{
+            res.json(obj);
+        }
+    });
 });
 
 router.delete('/objects/:uid', function(req, res) {
